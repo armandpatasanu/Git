0bcf2bfea4ab768fc56da5b30d084a867019857d
Armand <sebapatasanu@yahoo.com>
2020-03-18 19:32:07 +0200

    java file added

---
+++ program.java
@@ -0,0 +1,16 @@
+class Copil
+{
+    private int varsta;
+    private String nume;
+
+    public Copil(int v, String n)
+    {
+        this.varsta=v;
+        this.nume=n;
+    }
+
+    public int calculIQ
+    {
+        return 100;
+    }
+}
